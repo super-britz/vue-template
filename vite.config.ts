1cb18cea1bab254a775be7338c2b72cf3c77e797
梁胜 <906443185@qq.com>
2026-03-09 19:34:06 +0800

    feat: 集成 Element Plus 按需导入与自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +16,14 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+      dts: 'src/auto-imports.d.ts',
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+      dts: 'src/components.d.ts',
+    }),
   ],
   resolve: {
     alias: {
