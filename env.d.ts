82aa8dd30eeb0a4b899fb7694e29a73de1a39c48
梁胜 <906443185@qq.com>
2026-03-09 20:07:20 +0800

    fix: 拆分 .vue 类型声明为独立 ambient 文件修复 IDE 报错

--- env.d.ts
+++ env.d.ts
@@ -2,12 +2,6 @@
 
 export {}
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent
-  export default component
-}
-
 declare global {
   interface ImportMetaEnv {
     readonly VITE_API_BASE_URL: string
